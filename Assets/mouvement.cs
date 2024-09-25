b47c9e8be10cdd5e4c99b0f5f8a2b2a0f8ee398d
Yann479 <tetart.yann@gmail.com>
2024-09-25 20:49:15 +0200

    Ajout des commentaires

--- Assets/mouvement.cs
+++ Assets/mouvement.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class mouvement : MonoBehaviour
 {
-    private int saut = 0;
-    private int hauteur = 5;
+    private int saut = 0; // Compte le nombre de saut
+    private int hauteur = 5; // initialisation de la hauteur maximal du saut réalisable
     // Start is called before the first frame update
     void Start()
     {
@@ -31,18 +31,18 @@ public class mouvement : MonoBehaviour
         {
             transform.Rotate(Vector3.up, 2);
         }
-        if (Input.GetKey(KeyCode.Space) && saut<hauteur)
+        if (Input.GetKey(KeyCode.Space) && saut<hauteur) // Afin de ne pas pouvoir sauter en continu, la variable hauteur va limiter le nombre de saut consécutifs et ainsi contrôler la hauteur du saut
         {
-            transform.Translate(Vector3.up * 0.2f);
-            saut+=1;;
+            transform.Translate(Vector3.up * 0.2f); // Pour réaliser un saut il faut réaliser une translation en fonction de l'axe des Y, celle-ci sera répéte plusieurs fois en fonction de la valeur de la variable 'hauteur'
+            saut+=1;;   // Incrémentation de la variable 'saut' pour limiter la hauteur du saut et le rendre plus réaliste
         }
-        if(transform.position.y <= 1)
+        if(transform.position.y <= 1) // Vérification de la position du personnage pour savoir si il est sur le sol et donc si on l'autorise à nouveau à réaliser un saut
         {
-            saut=0;
+            saut=0; // Remise à 0, afin de lui permettre de sauter à nouveau
         }
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetKey(KeyCode.LeftShift)) // Utilisation de LeftShift afin de réaliser l'action de sprint avec la touche Shift
         {
-            transform.Translate(Vector3.back * 0.02f);
+            transform.Translate(Vector3.back * 0.02f); // Afin de réaliser un sprint, le simple fait de double la vitesse de déplacement par rapport à son déplacement via les flèches est suffissant (donc un passage de '0.01f' à '0.02f')
         }
     }
 }
--- Assets/poursuite.cs
+++ Assets/poursuite.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class poursuite : MonoBehaviour
 {
-    public Vector3 C;
-    public Vector3 S;
+    public Vector3 C; // Position du 'chat'
+    public Vector3 S; // Position de la 'souris'
     public float v;
     public GameObject cible;
     // Start is called before the first frame update
@@ -16,12 +16,11 @@ public class poursuite : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        C = this.transform.position;
-        S = cible.transform.position;
-        Vector3 PersoCible = -(cible.transform.position - transform.position);
-        if(PersoCible != Vector3.zero){
-            Quaternion rotationCible = Quaternion.LookRotation(PersoCible);
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotationCible, Time.deltaTime * 5f);
+        C = this.transform.position; // Mettre à jour la position du 'chat'
+        S = cible.transform.position; // Mettre à jour la position de la 'souris'
+        Vector3 PersoCible = -(cible.transform.position - transform.position); // Inversion de la position pour orienter le personnage 'chat' dans la direction du personnage 'souris' (et non qu'il lui tourne le dos)
+        if(PersoCible != Vector3.zero){ // vérification de l'emplacement du personnage 'souris' par rapport à l'orientation du pesonnage 'chat' pour savoir si il faut réaliser une modification
+            transform.rotation = Quaternion.LookRotation(PersoCible); // Modification de la rotation du personnage 'chat' pour faire face au personnage 'souris'
         }
     }
 }
